36d2e95169cc8e86b8a89656d94a14f5bc0fd22f
Alexandr Nadtoka <nadtoka.alexandr@gmail.com>
2018-11-19 12:56:42 +0200

    Init Single View App(Swift)

---
+++ MovieFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieFinder
+//
+//  Created by Alexandr Nadtoka on 11/19/18.
+//  Copyright © 2018 kreatimont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
